6e83fa4fde71fd72869f7d565a5d71274a1328b9
jordan-carlson99 <jordan.carlson.square@gmail.com>
2023-05-11 15:28:23 -0500

    created component folder structure

---
+++ src/components/MainContent.jsx
@@ -0,0 +1,13 @@
+import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
+import Body from "./Body/Body";
+
+export default function MainContent() {
+  return (
+    <>
+      <Header />
+      <Body />
+      <Footer />
+    </>
+  );
+}
