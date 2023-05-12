0879d22c286b4467d5956f8fa0356b1cbe5c9182
jordan-carlson99 <jordan.carlson.square@gmail.com>
2023-05-12 13:21:02 -0500

    synced components

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,3 +1,13 @@
+import TopFooter from "./TopFooter/TopFooter";
+import MidFooter from "./MidFooter/MidFooter";
+import BottomFooter from "./BottomFooter";
+
 export default function Footer() {
-  return <>Footer</>;
+  return (
+    <div id="footers">
+      <TopFooter />
+      <MidFooter />
+      <BottomFooter />
+    </div>
+  );
 }
