5060306b858a4ca4e0b7edf0bc9444cde292be39
jordan-carlson99 <jordan.carlson.square@gmail.com>
2023-05-12 17:41:05 -0500

    started gallery

--- client/src/components/Body/Gallery/Gallery.jsx
+++ client/src/components/Body/Gallery/Gallery.jsx
@@ -1,5 +1,41 @@
 import styles from "./Gallery.module.css";
 
 export default function Gallery() {
-  return <div className={styles["gallery-container"]}>Gallery</div>
+  const testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  return (
+    <div id="gallery">
+      {testArray.map((image) => {
+        return <ProductImage image={image} />;
+      })}
+    </div>
+  );
+}
+
+function ProductImage(props) {
+  return (
+    <div className={styles["gallery-container"]}>
+      {props.image}
+      <img className={styles["image"]}></img>
+    </div>
+  );
+}
+
+function ProductVideo(props) {
+  return (
+    <div>
+      <video
+        loop=""
+        playsInline=""
+        src={props.url}
+        onClick={(e) => {
+          pauseVideo(e);
+        }}
+        id="video-item"
+      ></video>
+    </div>
+  );
+}
+
+function pauseVideo(e) {
+  console.log("pause video");
 }
