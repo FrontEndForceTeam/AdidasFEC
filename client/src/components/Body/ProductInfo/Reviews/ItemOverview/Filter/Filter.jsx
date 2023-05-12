327dd58f1b774e9b55a01b6530be9e69f453ece6
jordan-carlson99 <jordan.carlson.square@gmail.com>
2023-05-12 14:20:47 -0500

    p info fix

---
+++ client/src/components/Body/ProductInfo/Reviews/ItemOverview/Filter/Filter.jsx
@@ -0,0 +1,5 @@
+import styles from "./Filter.module.css"
+
+export default function Filter() {
+  return <>Filter</>;
+}
---
+++ client/src/components/Body/ProductInfo/Reviews/ItemOverview/Sizing/Sizing.jsx
@@ -0,0 +1,5 @@
+import styles from "./Sizing.module.css";
+
+export default function Sizing() {
+    return <>Sizing</>
+}
