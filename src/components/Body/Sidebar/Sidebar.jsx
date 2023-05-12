c88a43d9dd1990344c6b6d521211034f8a9d6e4b
Spirit1269 <kimberlydjenkins1@gmail.com>
2023-05-12 11:01:29 -0500

    Added divs

---
+++ src/components/Body/Sidebar/Sidebar.jsx
@@ -0,0 +1,36 @@
+import react from 'react';
+import index from 'index.css';
+
+
+<div className = "Container">
+    <div className = "Description">
+
+    </div>
+
+
+    <duv className = "Customization">
+
+    </duv>
+
+
+    <div className = "Sizes">
+
+    </div>
+
+
+    <div className = "sizeGuide">
+
+    </div>
+
+
+    <div className = "addToBag">
+
+    </div>
+
+    
+    <div className = "footer">
+
+    </div>
+</div>
+
+export default Sidebar.jsx
