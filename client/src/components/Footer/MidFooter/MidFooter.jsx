0d9f41cde8e11e02f71a7a3cae9886400270dedb
jordan-carlson99 <jordan.carlson.square@gmail.com>
2023-05-12 15:40:57 -0500

    top-footer styling and mid footer fixes

--- client/src/components/Footer/MidFooter/MidFooter.jsx
+++ client/src/components/Footer/MidFooter/MidFooter.jsx
@@ -2,184 +2,172 @@ export default function MidFooter() {
   return (
     <footer id="mid-footer">
       <ul className="footer-list">
-        <li className="footer-category">
-          <h5>PRODUCTS</h5>
-          <li className="footer-item">
-            <a>Shoes</a>
-          </li>
-          <li className="footer-item">
-            <a>Clothing</a>
-          </li>
-          <li className="footer-item">
-            <a>Accessories</a>
-          </li>
-          <li className="footer-item">
-            <a>New Arrivals</a>
-          </li>
-          <li className="footer-item">
-            <a>Release Dates</a>
-          </li>
-          <li className="footer-item">
-            <a>Top Sellers</a>
-          </li>
-          <li className="footer-item">
-            <a>Outlet</a>
-          </li>
+        <h5>PRODUCTS</h5>
+        <li className="footer-item">
+          <a>Shoes</a>
+        </li>
+        <li className="footer-item">
+          <a>Clothing</a>
+        </li>
+        <li className="footer-item">
+          <a>Accessories</a>
+        </li>
+        <li className="footer-item">
+          <a>New Arrivals</a>
+        </li>
+        <li className="footer-item">
+          <a>Release Dates</a>
+        </li>
+        <li className="footer-item">
+          <a>Top Sellers</a>
+        </li>
+        <li className="footer-item">
+          <a>Outlet</a>
         </li>
       </ul>
       <ul className="footer-list">
-        <li className="footer-category">
-          <h5>SPORTS</h5>
-          <li className="footer-item">
-            <a>Football</a>
-          </li>
-          <li className="footer-item">
-            <a>Basketball</a>
-          </li>
-          <li className="footer-item">
-            <a>Golf</a>
-          </li>
-          <li className="footer-item">
-            <a>Gym & Training</a>
-          </li>
-          <li className="footer-item">
-            <a>Outdoor</a>
-          </li>
-          <li className="footer-item">
-            <a>Rugby</a>
-          </li>
-          <li className="footer-item">
-            <a>Running</a>
-          </li>
-          <li className="footer-item">
-            <a>Tennis</a>
-          </li>
+        <h5>SPORTS</h5>
+        <li className="footer-item">
+          <a>Football</a>
+        </li>
+        <li className="footer-item">
+          <a>Basketball</a>
+        </li>
+        <li className="footer-item">
+          <a>Golf</a>
+        </li>
+        <li className="footer-item">
+          <a>Gym & Training</a>
+        </li>
+        <li className="footer-item">
+          <a>Outdoor</a>
+        </li>
+        <li className="footer-item">
+          <a>Rugby</a>
+        </li>
+        <li className="footer-item">
+          <a>Running</a>
+        </li>
+        <li className="footer-item">
+          <a>Tennis</a>
         </li>
       </ul>
       <ul className="footer-list">
-        <li className="footer-category">
-          <h5>COLLECTIONS</h5>
-          <li className="footer-item">
-            <a>Originals</a>
-          </li>
-          <li className="footer-item">
-            <a>IVY PARK</a>
-          </li>
-          <li className="footer-item">
-            <a>Ultraboost</a>
-          </li>
-          <li className="footer-item">
-            <a>NMD</a>
-          </li>
-          <li className="footer-item">
-            <a>Gazelle</a>
-          </li>
-          <li className="footer-item">
-            <a>Superstar</a>
-          </li>
-          <li className="footer-item">
-            <a>Stan Smith</a>
-          </li>
-          <li className="footer-item">
-            <a>Ozweego</a>
-          </li>
+        <h5>COLLECTIONS</h5>
+        <li className="footer-item">
+          <a>Originals</a>
+        </li>
+        <li className="footer-item">
+          <a>IVY PARK</a>
+        </li>
+        <li className="footer-item">
+          <a>Ultraboost</a>
+        </li>
+        <li className="footer-item">
+          <a>NMD</a>
+        </li>
+        <li className="footer-item">
+          <a>Gazelle</a>
+        </li>
+        <li className="footer-item">
+          <a>Superstar</a>
+        </li>
+        <li className="footer-item">
+          <a>Stan Smith</a>
+        </li>
+        <li className="footer-item">
+          <a>Ozweego</a>
         </li>
       </ul>
       <ul className="footer-list">
-        <li className="footer-category">
-          <h5>SUPPORT</h5>
-          <li className="footer-item">
-            <a>Help</a>
-          </li>
-          <li className="footer-item">
-            <a>Returns & Refunds</a>
-          </li>
-          <li className="footer-item">
-            <a>Delivery</a>
-          </li>
-          <li className="footer-item">
-            <a>Size Charts</a>
-          </li>
-          <li className="footer-item">
-            <a>Store Finder</a>
-          </li>
-          <li className="footer-item">
-            <a>Sitemap</a>
-          </li>
-          <li className="footer-item">
-            <a>Student discount</a>
-          </li>
-          <li className="footer-item">
-            <a>Gift Cards</a>
-          </li>
-          <li className="footer-item">
-            <a>European Disputes Resolution</a>
-          </li>
-          <li className="footer-item">
-            <a>Imprint</a>
-          </li>
-          <li className="footer-item">
-            <a>Modern Slavery Act Transparency Statement</a>
-          </li>
+        <h5>SUPPORT</h5>
+        <li className="footer-item">
+          <a>Help</a>
+        </li>
+        <li className="footer-item">
+          <a>Returns & Refunds</a>
+        </li>
+        <li className="footer-item">
+          <a>Delivery</a>
+        </li>
+        <li className="footer-item">
+          <a>Size Charts</a>
+        </li>
+        <li className="footer-item">
+          <a>Store Finder</a>
+        </li>
+        <li className="footer-item">
+          <a>Sitemap</a>
+        </li>
+        <li className="footer-item">
+          <a>Student discount</a>
+        </li>
+        <li className="footer-item">
+          <a>Gift Cards</a>
+        </li>
+        <li className="footer-item">
+          <a>European Disputes Resolution</a>
+        </li>
+        <li className="footer-item">
+          <a>Imprint</a>
+        </li>
+        <li className="footer-item">
+          <a>Modern Slavery Act Transparency Statement</a>
         </li>
       </ul>
       <ul className="footer-list">
-        <li className="footer-category">
-          <h5>COMPANY INFO</h5>
-          <li className="footer-item">
-            <a>About Us</a>
-          </li>
-          <li className="footer-item">
-            <a>Careers</a>
-          </li>
-          <li className="footer-item">
-            <a>Press</a>
-          </li>
-          <li className="footer-item">
-            <a>Mobile Apps</a>
-          </li>
-          <li className="footer-item">
-            <a>adidas Confirmed</a>
-          </li>
-          <li className="footer-item">
-            <a>adidas Blog</a>
-          </li>
-          <li className="footer-item">
-            <a>adidas News</a>
-          </li>
-          <li className="footer-item">
-            <a>Affiliate Program</a>
-          </li>
-          <li className="footer-item">
-            <a>Location Selector </a>
-          </li>
-          <h5>MORE ADIDAS</h5>
-          <li className="footer-item">
-            <a>Training Equipment</a>
-          </li>
+        <h5>COMPANY INFO</h5>
+        <li className="footer-item">
+          <a>About Us</a>
+        </li>
+        <li className="footer-item">
+          <a>Careers</a>
+        </li>
+        <li className="footer-item">
+          <a>Press</a>
+        </li>
+        <li className="footer-item">
+          <a>Mobile Apps</a>
+        </li>
+        <li className="footer-item">
+          <a>adidas Confirmed</a>
+        </li>
+        <li className="footer-item">
+          <a>adidas Blog</a>
+        </li>
+        <li className="footer-item">
+          <a>adidas News</a>
+        </li>
+        <li className="footer-item">
+          <a>Affiliate Program</a>
+        </li>
+        <li className="footer-item">
+          <a>Location Selector </a>
+        </li>
+        <h5>MORE ADIDAS</h5>
+        <li className="footer-item">
+          <a>Training Equipment</a>
         </li>
       </ul>
       <ul id="footer-list">
-        <li className="footer-category">
-          <h5>FOLLOW US</h5>
-          <li className="footer-item">
-            <img>Facebook</img>
-          </li>
-          <li className="footer-item">
-            <img>Insta</img>
-          </li>
-          <li className="footer-item">
-            <img>Twitter</img>
-          </li>
-          <li className="footer-item">
-            <img>Pinterest</img>
-          </li>
-          <li className="footer-item">
-            <img>Tiktok</img>
-          </li>
-          <li className="footer-item">
-            <img>Youtube</img>
-          </li>
+        <h5>FOLLOW US</h5>
+        <li className="footer-item">
+          <img alt="Facebook"></img>
+        </li>
+        <li className="footer-item">
+          <img alt="Insta"></img>
+        </li>
+        <li className="footer-item">
+          <img src="Twitter"></img>
+        </li>
+        <li className="footer-item">
+          <img src="Pinterest"></img>
+        </li>
+        <li className="footer-item">
+          <img src="Tiktok"></img>
+        </li>
+        <li className="footer-item">
+          <img src="Youtube"></img>
         </li>
       </ul>
     </footer>
--- client/src/components/Footer/TopFooter/TopFooter.jsx
+++ client/src/components/Footer/TopFooter/TopFooter.jsx
@@ -1,13 +1,30 @@
+import styles from "./TopFooter.module.css";
+
 export default function TopFooter() {
   return (
-    <div id="pre-footer">
-      <span id="membr-message">BECOME A MEMBER & GET 10% OFF*</span>
-      <button id="sign-up btn">
-        <span>SIGN UP FOR FREE</span>
-        <div id="img-container">
-          <img id="arrow"></img>
-        </div>
+    <div id={styles["footer-container"]}>
+      <div id={styles["feedback"]} className={styles["phone"]}>
+        <h5 className={styles["feedback-text"]}>YOUR OPINION COUNTS</h5>
+        <p className={styles["feedback-text"]}>
+          We strive to serve you better and appreciate your feedback
+        </p>
+      </div>
+      <button type="button" id="back-to-btn">
+        BACK TO TOP
       </button>
+      <div id={styles["ctrl-container"]}>
+        <button>LOGIN</button>
+        <button>YOUR BAG (0)</button>
+      </div>
+      <div id={styles["pre-footer"]}>
+        <span id="member-message">BECOME A MEMBER & GET 10% OFF*</span>
+        <button id="sign-up btn">
+          <span>SIGN UP FOR FREE</span>
+          <div id="img-container">
+            <img id="arrow"></img>
+          </div>
+        </button>
+      </div>
     </div>
   );
 }
