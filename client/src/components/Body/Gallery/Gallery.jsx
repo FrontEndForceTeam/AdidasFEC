1c7a1864f5645a04beb90be12473f96afbc903e6
jordan-carlson99 <jordan.carlson.square@gmail.com>
2023-05-15 10:32:25 -0500

    added product vidoe and show more button + styles

--- client/src/components/Body/Gallery/Gallery.jsx
+++ client/src/components/Body/Gallery/Gallery.jsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
 import styles from "./Gallery.module.css";
 
 export default function Gallery() {
   const testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const [showBtn, setShowBtn] = useState("Show More");
   return (
-    <div id="gallery">
-      {testArray.map((image) => {
+    <div id={styles["gallery"]}>
+      {testArray.map((image, index) => {
+        if (image.type == "video") {
+          return <ProductVideo url={image.url} />;
+        } else if (index == 4) {
+          return (
+            <>
+              <ShowMoreButton show={showBtn} />
+              <ProductImage image={image} />
+            </>
+          );
+        }
         return <ProductImage image={image} />;
       })}
     </div>
@@ -36,6 +48,19 @@ function ProductVideo(props) {
   );
 }
 
+function ShowMoreButton(props) {
+  return (
+    <button>
+      <span>{props.show}</span>
+      <img alt="[arrow icon]"></img>
+    </button>
+  );
+}
+
 function pauseVideo(e) {
   console.log("pause video");
+}
+
+function showMore(e) {
+  console.log("show more");
 }
