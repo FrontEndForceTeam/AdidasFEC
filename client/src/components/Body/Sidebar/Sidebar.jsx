b0c0a0db8b6a91b59c4ac4a03b129ff4eaf5e114
Spirit1269 <kimberlydjenkins1@gmail.com>
2023-05-15 17:52:54 -0500

    Commit 1

--- client/src/components/Body/Sidebar/Sidebar.jsx
+++ client/src/components/Body/Sidebar/Sidebar.jsx
@@ -1,9 +1,23 @@
 import styles from "./Sidebar.module.css";
+// import Reviews from "./ProductInfo/Reviews.jsx"
+ //import "AdhausDIN-Condtia" from "./src/fonts";
 
 export default function Sidebar() {
   return (
     <div className={styles['sidebar-container']}> 
-     sidebar
+      <div className = "description">
+        <div className = "preHeader">
+          <div className = "productCategory">
+            <span > Originals</span>
+          </div>
+          <button className="reviewCount" link = ".ProductInfo/reviews">* * * *  434</button>
+        </div>
+        <h1 className = "productTitle">
+          <span> ZX 5K  BOOST SHOES</span>
+          </h1>
+        <div className = "price">84.50</div>
+        <div className = "colorName">Core "Black / Core Black / Grey Six</div>
+      </div>
     </div>
   );
 }
