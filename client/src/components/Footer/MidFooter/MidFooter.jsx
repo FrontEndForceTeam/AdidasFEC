adab4dc95b69adbb0f54244338dca53c85a7de48
jordan-carlson99 <jordan.carlson.square@gmail.com>
2023-05-15 11:46:10 -0500

    added links to middle footer

--- client/src/components/Footer/MidFooter/MidFooter.jsx
+++ client/src/components/Footer/MidFooter/MidFooter.jsx
@@ -5,170 +5,188 @@ export default function MidFooter() {
       <ul className={styles["footer-category"]}>
         <h5>PRODUCTS</h5>
         <li className={styles["footer-item"]}>
-          <a>Shoes</a>
+          <a href="https://www.adidas.co.uk/shoes">Shoes</a>
         </li>
         <li className={styles["footer-item"]}>
-          <a>Clothing</a>
+          <a href="https://www.adidas.co.uk/clothing">Clothing</a>
         </li>
         <li className={styles["footer-item"]}>
-          <a>Accessories</a>
+          <a href="https://www.adidas.co.uk/accessories">Accessories</a>
         </li>
         <li className={styles["footer-item"]}>
-          <a>New Arrivals</a>
+          <a href="https://www.adidas.co.uk/new_arrivals">New Arrivals</a>
         </li>
         <li className={styles["footer-item"]}>
-          <a>Release Dates</a>
+          <a href="https://www.adidas.co.uk/release_dates">Release Dates</a>
         </li>
         <li className={styles["footer-item"]}>
-          <a>Top Sellers</a>
+          <a href="https://www.adidas.co.uk/top_sellers">Top Sellers</a>
         </li>
         <li className={styles["footer-item"]}>
-          <a>Outlet</a>
+          <a href="https://www.adidas.co.uk/outlet">Outlet</a>
         </li>
       </ul>
       <ul className={styles["footer-category"]}>
         <h5>SPORTS</h5>
         <li className={styles["footer-item"]}>
-          <a>Football</a>
+          <a href="https://www.adidas.co.uk/football">Football</a>
         </li>
         <li className={styles["footer-item"]}>
-          <a>Basketball</a>
+          <a href="https://www.adidas.co.uk/basketball">Basketball</a>
         </li>
         <li className={styles["footer-item"]}>
-          <a>Golf</a>
+          <a href="https://www.adidas.co.uk/golf">Golf</a>
         </li>
         <li className={styles["footer-item"]}>
-          <a>Gym & Training</a>
+          <a href="https://www.adidas.co.uk/gym_training">Gym & Training</a>
         </li>
         <li className={styles["footer-item"]}>
-          <a>Outdoor</a>
+          <a href="https://www.adidas.co.uk/outdoor">Outdoor</a>
         </li>
         <li className={styles["footer-item"]}>
-          <a>Rugby</a>
+          <a href="https://www.adidas.co.uk/rugby">Rugby</a>
         </li>
         <li className={styles["footer-item"]}>
-          <a>Running</a>
+          <a href="https://www.adidas.co.uk/running">Running</a>
         </li>
         <li className={styles["footer-item"]}>
-          <a>Tennis</a>
+          <a href="https://www.adidas.co.uk/tennis">Tennis</a>
         </li>
       </ul>
       <ul className={styles["footer-category"]}>
         <h5>COLLECTIONS</h5>
         <li className={styles["footer-item"]}>
-          <a>Originals</a>
+          <a href="https://www.adidas.co.uk/originals">Originals</a>
         </li>
         <li className={styles["footer-item"]}>
-          <a>IVY PARK</a>
+          <a href="https://www.adidas.co.uk/ivypark">IVY PARK</a>
         </li>
         <li className={styles["footer-item"]}>
-          <a>Ultraboost</a>
+          <a href="https://www.adidas.co.uk/ultraboost">Ultraboost</a>
         </li>
         <li className={styles["footer-item"]}>
-          <a>NMD</a>
+          <a href="https://www.adidas.co.uk/nmd">NMD</a>
         </li>
         <li className={styles["footer-item"]}>
-          <a>Gazelle</a>
+          <a href="https://www.adidas.co.uk/gazelle">Gazelle</a>
         </li>
         <li className={styles["footer-item"]}>
-          <a>Superstar</a>
+          <a href="https://www.adidas.co.uk/superstar">Superstar</a>
         </li>
         <li className={styles["footer-item"]}>
-          <a>Stan Smith</a>
+          <a href="https://www.adidas.co.uk/stan_smith">Stan Smith</a>
         </li>
         <li className={styles["footer-item"]}>
-          <a>Ozweego</a>
+          <a href="https://www.adidas.co.uk/ozweego">Ozweego</a>
         </li>
       </ul>
       <ul className={styles["footer-category"]}>
         <h5>SUPPORT</h5>
         <li className={styles["footer-item"]}>
-          <a>Help</a>
+          <a href="https://www.adidas.co.uk/help">Help</a>
         </li>
         <li className={styles["footer-item"]}>
-          <a>Returns & Refunds</a>
+          <a href="https://www.adidas.co.uk/help/returns-refunds">
+            Returns & Refunds
+          </a>
         </li>
         <li className={styles["footer-item"]}>
-          <a>Delivery</a>
+          <a href="https://www.adidas.co.uk/help?hcid=DELIVERY">Delivery</a>
         </li>
         <li className={styles["footer-item"]}>
-          <a>Size Charts</a>
+          <a href="https://www.adidas.co.uk/help?hcid=SIZE_GUIDE">
+            Size Charts
+          </a>
         </li>
         <li className={styles["footer-item"]}>
-          <a>Store Finder</a>
+          <a href="https://www.adidas.co.uk/storefinder">Store Finder</a>
         </li>
         <li className={styles["footer-item"]}>
-          <a>Sitemap</a>
+          <a href="https://www.adidas.co.uk/help-topics-sitemap.html">
+            Sitemap
+          </a>
         </li>
         <li className={styles["footer-item"]}>
-          <a>Student discount</a>
+          <a href="https://www.adidas.co.uk/student-discount">
+            Student discount
+          </a>
         </li>
         <li className={styles["footer-item"]}>
-          <a>Gift Cards</a>
+          <a href="https://www.adidas.co.uk/gift_cards">Gift Cards</a>
         </li>
         <li className={styles["footer-item"]}>
-          <a>European Disputes Resolution</a>
+          <a href="https://www.adidas.co.uk/european-disputes-resolution.html">
+            European Disputes Resolution
+          </a>
         </li>
         <li className={styles["footer-item"]}>
-          <a>Imprint</a>
+          <a href="https://www.adidas.co.uk/help-topics-imprint.html">
+            Imprint
+          </a>
         </li>
         <li className={styles["footer-item"]}>
-          <a>Modern Slavery Act Transparency Statement</a>
+          <a href="https://www.adidas.co.uk/shoes">
+            Modern Slavery Act Transparency Statement
+          </a>
         </li>
       </ul>
       <ul className={styles["footer-category"]}>
         <h5>COMPANY INFO</h5>
         <li className={styles["footer-item"]}>
-          <a>About Us</a>
+          <a href="https://www.adidas-group.com/en/">About Us</a>
         </li>
         <li className={styles["footer-item"]}>
-          <a>Careers</a>
+          <a href="https://careers.adidas-group.com/?brand=adidas">Careers</a>
         </li>
         <li className={styles["footer-item"]}>
-          <a>Press</a>
+          <a href="https://news.adidas.com/">Press</a>
         </li>
         <li className={styles["footer-item"]}>
-          <a>Mobile Apps</a>
+          <a href="https://www.adidas.co.uk/mobileapps">Mobile Apps</a>
         </li>
         <li className={styles["footer-item"]}>
-          <a>adidas Confirmed</a>
+          <a href="https://www.adidas.co.uk/confirmed">adidas Confirmed</a>
         </li>
         <li className={styles["footer-item"]}>
-          <a>adidas Blog</a>
+          <a href="https://www.adidas.co.uk/blog">adidas Blog</a>
         </li>
         <li className={styles["footer-item"]}>
-          <a>adidas News</a>
+          <a href="https://news.adidas.com/">adidas News</a>
         </li>
         <li className={styles["footer-item"]}>
-          <a>Affiliate Program</a>
+          <a href="https://www.adidas.co.uk/help/company-information/adidas-affiliates-program">
+            Affiliate Program
+          </a>
         </li>
         <li className={styles["footer-item"]}>
-          <a>Location Selector </a>
+          <a href="https://www.adidas.co.uk/location-selector.html">
+            Location Selector{" "}
+          </a>
         </li>
         <h5>MORE ADIDAS</h5>
         <li className={styles["footer-item"]}>
-          <a>Training Equipment</a>
+          <a href="https://www.adidashardware.com/">Training Equipment</a>
         </li>
       </ul>
       <ul id={styles["footer-category"]}>
         <h5>FOLLOW US</h5>
         <li className={styles["footer-item"]}>
-          <img alt="Facebook"></img>
+          <img alt="Facebook" href="https://www.facebook.com/adidas"></img>
         </li>
         <li className={styles["footer-item"]}>
-          <img alt="Insta"></img>
+          <img alt="Insta" href="https://www.instagram.com/adidas/"></img>
         </li>
         <li className={styles["footer-item"]}>
-          <img src="Twitter"></img>
+          <img src="Twitter" href="https://twitter.com/adidas"></img>
         </li>
         <li className={styles["footer-item"]}>
-          <img src="Pinterest"></img>
+          <img src="Pinterest" href="https://www.pinterest.de/adidas/"></img>
         </li>
         <li className={styles["footer-item"]}>
-          <img src="Tiktok"></img>
+          <img src="Tiktok" href="https://www.tiktok.com/@adidas?lang=en"></img>
         </li>
         <li className={styles["footer-item"]}>
-          <img src="Youtube"></img>
+          <img src="Youtube" href="https://www.youtube.com/c/adidas"></img>
         </li>
       </ul>
     </footer>
