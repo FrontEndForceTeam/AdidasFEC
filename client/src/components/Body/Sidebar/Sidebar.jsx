cb19254515760c19cb3414fefe7cc2003804fe0d
Spirit1269 <kimberlydjenkins1@gmail.com>
2023-05-15 19:42:06 -0500

    Sidebar Description Complete

--- client/src/components/Body/Sidebar/Sidebar.jsx
+++ client/src/components/Body/Sidebar/Sidebar.jsx
@@ -1,22 +1,23 @@
 import styles from "./Sidebar.module.css";
-// import Reviews from "./ProductInfo/Reviews.jsx"
- //import "AdhausDIN-Condtia" from "./src/fonts";
 
 export default function Sidebar() {
   return (
-    <div className={styles['sidebar-container']}> 
-      <div className = "description">
-        <div className = "preHeader">
-          <div className = "productCategory">
-            <span > Originals</span>
+    <div className={styles['sidebar-container']}>
+      <div className={styles.description}>
+        <div className={styles.preHeader}>
+          <div className={styles.productCategory}>
+            <span>Originals</span>
           </div>
-          <button className="reviewCount" link = ".ProductInfo/reviews">* * * *  434</button>
+          <button className={styles.reviewCount} link=".ProductInfo/reviews">* * * * *  434</button>
         </div>
-        <h1 className = "productTitle">
-          <span> ZX 5K  BOOST SHOES</span>
-          </h1>
-        <div className = "price">84.50</div>
-        <div className = "colorName">Core "Black / Core Black / Grey Six</div>
+        <h1 className={styles.productTitle}>
+          <span>ZX 5K BOOST SHOES</span>
+        </h1>
+        <div className={styles.priceWrapper}>
+          <div className={styles.price}><s>130 </s></div>
+          <div className={styles.discountPrice}>84.50</div>
+        </div>
+        <div className={styles.colorName}>Core Black / Core Black / Grey Six</div>
       </div>
     </div>
   );
