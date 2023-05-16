82f3b14fd22f9305c3b9594f4785a13d5cbd2c46
Jacob Phillips <0123phillips@gmail.com>
2023-05-16 18:02:28 +0100

    adds animation for preheader, and overall menu items for the main header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,7 @@
-import { useState } from "react";
 import "./App.css";
 import MainContent from "./components/MainContent";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
