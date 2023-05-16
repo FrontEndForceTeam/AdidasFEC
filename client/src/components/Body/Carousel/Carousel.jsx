df5a2c5fb199c6e9d7ac7a9bc0b1315c1149dc76
jordan-carlson99 <jordan.carlson.square@gmail.com>
2023-05-16 13:23:09 -0500

    got carouselt proportions correct

--- client/src/components/Body/Carousel/Carousel.jsx
+++ client/src/components/Body/Carousel/Carousel.jsx
@@ -1,23 +1,39 @@
 import styles from "./Carousel.module.css";
 
 export default function Carousel(props) {
-  const testData = [{ img: 1 }, { img: 2 }, { img: 3 }, { img: 4 }, { img: 5 }];
+  const testData = [
+    { img: 1 },
+    { img: 2 },
+    { img: 3 },
+    { img: 4 },
+    { img: 5 },
+    { img: 6 },
+    { img: 7 },
+    { img: 8 },
+  ];
   return (
-    <div className={styles["carousel-container"]}>
-      {testData.map((image) => {
-        return <CarouselElement img={image} />;
-      })}
+    <div className={styles["carousel-block"]}>
+      <h2>Title Category</h2>
+      <div className={styles["carousel-container"]}>
+        {testData.map((image) => {
+          return <CarouselElement img={image} />;
+        })}
+      </div>
     </div>
   );
 }
 function CarouselElement(props) {
   return (
     <div className={styles["carousel-item"]}>
-      <img alt={props.image} />
-      <img alt="icon" className="icon" />
-      <p className={styles["price"]}>$10</p>
-      <h5>title</h5>
-      <p>description</p>
+      <div className={styles["card-top"]}>
+        <img alt={props.image} />
+        <img alt="icon" className="icon" />
+        <p className={styles["price"]}>$10</p>
+      </div>
+      <div className={styles["card-bottom"]}>
+        <h5>title</h5>
+        <p>description</p>
+      </div>
     </div>
   );
 }
