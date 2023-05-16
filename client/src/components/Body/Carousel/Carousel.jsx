207893750b445be57e58ea25d4f967a535487ceb
jordan-carlson99 <jordan.carlson.square@gmail.com>
2023-05-16 17:27:37 -0500

    swiper framework for carousel

--- client/src/components/Body/Carousel/Carousel.jsx
+++ client/src/components/Body/Carousel/Carousel.jsx
@@ -1,4 +1,14 @@
 import styles from "./Carousel.module.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { useRef } from "react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/scrollbar";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+import { Keyboard, Scrollbar, Navigation, Pagination } from "swiper";
 
 export default function Carousel(props) {
   const testData = [
@@ -11,23 +21,72 @@ export default function Carousel(props) {
     { img: 7 },
     { img: 8 },
   ];
+  const navigationPrevRef = useRef(null);
+  const navigationNextRef = useRef(null);
   return (
-    <div className={styles["carousel-block"]}>
-      <h2>Title Category</h2>
-      <div className={styles["carousel-container"]}>
+    <>
+      <Swiper
+        slidesPerView={4}
+        centeredSlides={false}
+        slidesPerGroup={3}
+        // grabCursor={true}
+        scrollbar={true}
+        navigation={{
+          prevEl: navigationPrevRef.current,
+          nextEl: navigationNextRef.current,
+        }}
+        modules={[Keyboard, Scrollbar, Navigation, Pagination]}
+        className={styles["swiper"]}
+      >
         {testData.map((image) => {
-          return <CarouselElement img={image} />;
+          return (
+            <SwiperSlide className="test">
+              <div className={styles["product-card"]}>{image.img}</div>
+            </SwiperSlide>
+          );
         })}
-      </div>
-    </div>
+      </Swiper>
+      <svg
+        ref={navigationNextRef}
+        className={styles["carousel-btn-next"]}
+        onClick={() => {
+          console.log("forward");
+        }}
+      >
+        <path
+          d="m17.59 7 5 5-5 5M0 12h22"
+          fill="none"
+          stroke="currentColor"
+          strokeMiterlimit="10"
+          strokeWidth="2"
+        ></path>
+      </svg>
+      <svg
+        ref={navigationPrevRef}
+        className={styles["carousel-btn-prev"]}
+        onClick={() => {
+          console.log("backward");
+        }}
+      >
+        <path
+          fill="none"
+          stroke="currentColor"
+          stroke-iterlimit="10"
+          strokeWidth="2"
+          d="m6.4 17-5-5 5-5M2 12h22"
+        ></path>
+      </svg>
+    </>
   );
 }
+
 function CarouselElement(props) {
   return (
     <div className={styles["carousel-item"]}>
-      <div className={styles["card-top"]}>
-        <img alt={props.image} />
-        <img alt="icon" className="icon" />
+      <div
+        className={styles["card-top"]}
+        style={{ backgroundImage: props.image }}
+      >
         <p className={styles["price"]}>$10</p>
       </div>
       <div className={styles["card-bottom"]}>
