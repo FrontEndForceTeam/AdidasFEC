bd28042f3fb572f68e82188732ab5b64252c7cb0
jordan-carlson99 <jordan.carlson.square@gmail.com>
2023-05-19 14:17:19 -0500

    removed phone view items from footer

--- client/src/components/Footer/TopFooter/TopFooter.jsx
+++ client/src/components/Footer/TopFooter/TopFooter.jsx
@@ -3,19 +3,6 @@ import styles from "./TopFooter.module.css";
 export default function TopFooter() {
   return (
     <div id={styles["footer-container"]}>
-      <div id={styles["feedback"]} className={styles["phone"]}>
-        <h5 className={styles["feedback-text"]}>YOUR OPINION COUNTS</h5>
-        <p className={styles["feedback-text"]}>
-          We strive to serve you better and appreciate your feedback
-        </p>
-      </div>
-      <button type="button" id="back-to-btn">
-        BACK TO TOP
-      </button>
-      <div id={styles["ctrl-container"]}>
-        <button className={styles["ctrl-btn"]}>LOGIN</button>
-        <button className={styles["ctrl-btn"]}>YOUR BAG (0)</button>
-      </div>
       <div id={styles["pre-footer"]}>
         <span id={styles["member-message"]}>
           BECOME A MEMBER & GET 10% OFF*
