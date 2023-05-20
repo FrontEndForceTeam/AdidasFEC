290cd3a224adf51c58a9788fe770adf92462edbb
Jacob Phillips <0123phillips@gmail.com>
2023-05-20 01:36:03 +0100

    adjusted react router

--- client/src/components/MainContent.jsx
+++ client/src/components/MainContent.jsx
@@ -1,36 +1,15 @@
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
 import Body from "./Body/Body";
-import { useState } from "react";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  useNavigate,
-} from "react-router-dom";
 
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <Body />,
-//   },
-// ]);
 
-export default function MainContent() {
-  const [shoe, setShoe] = useState(1);
-  const handleShoeChange = (id) => {
-    setShoe(id);
-  };
 
-  const router = createBrowserRouter([
-    {
-      path: `/${shoe}`,
-      element: <Body shoe={shoe} handleShoeChange={handleShoeChange} />,
-    },
-  ]);
+export default function MainContent({shoe, handleShoeChange}) {
+ 
   return (
     <>
       <Header />
-      <RouterProvider router={router} />
+      <Body shoe={shoe} handleShoeChange={handleShoeChange} />
       <Footer />
     </>
   );
